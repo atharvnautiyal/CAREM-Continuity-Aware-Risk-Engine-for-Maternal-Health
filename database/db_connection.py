029b504d0f06812c25c871059036b93cee28b390
atharvnautiyal <work.atharvnautiyal@gmail.com>
2026-06-06 20:57:33 +0530

    Update db_connection.py

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,15 +1,21 @@
-import mysql.connector
-from mysql.connector import Error
+# import mysql.connector
+# from mysql.connector import Error
+
+# def get_connection():
+#     try:
+#         conn = mysql.connector.connect(
+#             host = "localhost",
+#             user = "root",
+#             password = "123@123",
+#             database = "carem_db"
+#         )
+#         return conn
+#     except Error as e:
+#         print("Error connecting to MySQL:", e)
+#         return None 
+
+import sqlite3
 
 def get_connection():
-    try:
-        conn = mysql.connector.connect(
-            host = "localhost",
-            user = "root",
-            password = "123@123",
-            database = "carem_db"
-        )
-        return conn
-    except Error as e:
-        print("Error connecting to MySQL:", e)
-        return None 
+    conn = sqlite3.connect("carem.db")
+    return conn
